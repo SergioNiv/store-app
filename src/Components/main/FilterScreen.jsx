76b180d7bcfcc4bd6c5d08115c84c5ac57ac78b9
Sergio Nivin <nivin684@gmail.com>
2021-08-08 23:19:10 -0500

    filter component, order component, difuminado oscuro

--- src/Components/main/FilterScreen.jsx
+++ src/Components/main/FilterScreen.jsx
@@ -22,164 +22,180 @@ export const FilterScreen = () => {
 				<div onClick={handleActiveFilter} className="type__title-container">
 					<span className="type__title">Filtrar</span>
 				</div>
-				<ul className={`type__menu ${filterState && 'activeFilterType'}`}>
-					<button onClick={handleActiveFilter} className="btn__back">
-						Atrás
-					</button>
-					<details className="details__container" open>
-						<summary className="type__menu-link">Tipo</summary>
-						<ul className="type__submenu">
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByType('polo', gender))}
-							>
-								Polo
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByType('sueter', gender))}
-							>
-								Sueter
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() =>
-									dispatch(filterClothesByType('pantalones', gender))
-								}
-							>
-								Pantalón
-							</li>
-							{gender === 'mujer' ? (
+				<div
+					className={`type__menu-black ${filterState && 'activeFilterType'}`}
+				>
+					<ul className="type__menu">
+						<button onClick={handleActiveFilter} className="btn__back">
+							Atrás
+						</button>
+						<details className="details__container" open>
+							<summary className="type__menu-link">Tipo</summary>
+							<ul className="type__submenu">
+								<li
+									className="type__submenu-link"
+									onClick={() => dispatch(filterClothesByType('polo', gender))}
+								>
+									Polo
+								</li>
 								<li
 									className="type__submenu-link"
 									onClick={() =>
-										dispatch(filterClothesByType('blusas', gender))
+										dispatch(filterClothesByType('sueter', gender))
 									}
 								>
-									Blusa
+									Sueter
 								</li>
-							) : (
 								<li
 									className="type__submenu-link"
 									onClick={() =>
-										dispatch(filterClothesByType('blusas', gender))
+										dispatch(filterClothesByType('pantalones', gender))
 									}
 								>
-									Camisa
+									Pantalón
 								</li>
-							)}
-						</ul>
-					</details>
+								{gender === 'mujer' ? (
+									<li
+										className="type__submenu-link"
+										onClick={() =>
+											dispatch(filterClothesByType('blusas', gender))
+										}
+									>
+										Blusa
+									</li>
+								) : (
+									<li
+										className="type__submenu-link"
+										onClick={() =>
+											dispatch(filterClothesByType('blusas', gender))
+										}
+									>
+										Camisa
+									</li>
+								)}
+							</ul>
+						</details>
 
-					<details className="details__container" open>
-						<summary className="type__menu-link">Color</summary>
-						<ul className="type__submenu">
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Negro', gender))}
-							>
-								Negro
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() =>
-									dispatch(filterClothesByColor('Turquesa', gender))
-								}
-							>
-								Turquesa
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() =>
-									dispatch(filterClothesByColor('Amarillo', gender))
-								}
-							>
-								Amarillo
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Blanco', gender))}
-							>
-								Blanco
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() =>
-									dispatch(filterClothesByColor('Celeste', gender))
-								}
-							>
-								Celeste
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Verde', gender))}
-							>
-								Verde
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Azul', gender))}
-							>
-								Azul
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Café', gender))}
-							>
-								Café
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Gris', gender))}
-							>
-								Gris
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Rojo', gender))}
-							>
-								Rojo
-							</li>
-							<li
-								className="type__submenu-link"
-								onClick={() => dispatch(filterClothesByColor('Piel', gender))}
-							>
-								Piel
-							</li>
-						</ul>
-					</details>
-				</ul>
+						<details className="details__container" open>
+							<summary className="type__menu-link">Color</summary>
+							<ul className="type__submenu">
+								<li
+									className="type__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByColor('Negro', gender))
+									}
+								>
+									Negro
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByColor('Turquesa', gender))
+									}
+								>
+									Turquesa
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByColor('Amarillo', gender))
+									}
+								>
+									Amarillo
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByColor('Blanco', gender))
+									}
+								>
+									Blanco
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByColor('Celeste', gender))
+									}
+								>
+									Celeste
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByColor('Verde', gender))
+									}
+								>
+									Verde
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() => dispatch(filterClothesByColor('Azul', gender))}
+								>
+									Azul
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() => dispatch(filterClothesByColor('Café', gender))}
+								>
+									Café
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() => dispatch(filterClothesByColor('Gris', gender))}
+								>
+									Gris
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() => dispatch(filterClothesByColor('Rojo', gender))}
+								>
+									Rojo
+								</li>
+								<li
+									className="type__submenu-link"
+									onClick={() => dispatch(filterClothesByColor('Piel', gender))}
+								>
+									Piel
+								</li>
+							</ul>
+						</details>
+					</ul>
+				</div>
 			</div>
 			<div className="filter__order">
 				<div onClick={handleActiveOrder} className="order__title-container">
 					<span className="order__title">Ordenar</span>
 				</div>
-				<ul className={`order__menu ${orderState && 'activeFilterType'}`}>
-					<button onClick={handleActiveOrder} className="btn__back">
-						Atrás
-					</button>
-					<details className="details__container" open>
-						<summary className="order__menu-link">Por precio</summary>
-						<ul className="order__submenu">
-							<li
-								className="order__submenu-link"
-								onClick={() =>
-									dispatch(filterClothesByPrice('ascendente', gender))
-								}
-							>
-								Ascendente
-							</li>
-							<li
-								className="order__submenu-link"
-								onClick={() =>
-									dispatch(filterClothesByPrice('descendente', gender))
-								}
-							>
-								Descendente
-							</li>
-						</ul>
-					</details>
-				</ul>
+				<div
+					className={`order__menu-black ${orderState && 'activeFilterType'}`}
+				>
+					<ul className="order__menu">
+						<button onClick={handleActiveOrder} className="btn__back">
+							Atrás
+						</button>
+						<details className="details__container" open>
+							<summary className="order__menu-link">Por precio</summary>
+							<ul className="order__submenu">
+								<li
+									className="order__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByPrice('ascendente', gender))
+									}
+								>
+									Ascendente
+								</li>
+								<li
+									className="order__submenu-link"
+									onClick={() =>
+										dispatch(filterClothesByPrice('descendente', gender))
+									}
+								>
+									Descendente
+								</li>
+							</ul>
+						</details>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
