2de865f908f048404ab77c47846842d0155c63b0
Sergio Nivin <nivin684@gmail.com>
2021-08-19 21:38:05 -0500

    diseño navbar, refactorizado

--- src/Components/navbar/NavLogout.jsx
+++ src/Components/navbar/NavLogout.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Startlogout } from '../../actions/auth';
 import { clothesLogoutCleaning } from '../../actions/filter';
 import { isLoggedEnd } from '../../actions/ui';
 
 export const NavLogout = () => {
+	const { name } = useSelector((state) => state.ui);
+
 	const dispatch = useDispatch();
+
 	const handleLogout = () => {
 		dispatch(Startlogout());
 		dispatch(isLoggedEnd());
 		dispatch(clothesLogoutCleaning());
 	};
+
 	return (
 		<Link to="/auth/login" className="nav__login" onClick={handleLogout}>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="22"
-				height="22"
-				fill="currentColor"
-				className="bi bi-door-open"
-				viewBox="0 0 16 16"
-			>
-				<path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
-				<path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
-			</svg>
+			<i className="fas fa-sign-in-alt"></i>
+			<span className="nav__login-name">{name ? name : 'Ingresar'}</span>
 		</Link>
 	);
 };
--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -8,123 +8,150 @@ import { useDispatch, useSelector } from 'react-redux';
 import { filterSearchMenu } from '../../actions/filter';
 
 export const Navbar = () => {
-	const { name: displayName } = useSelector((state) => state.ui);
+	const [activeOpacity, setActiveOpacity] = useState(false);
+	const [btnMenu, setBtnMenu] = useState(false);
+	const [btnSearch, setBtnSearch] = useState(false);
+
 	const { cartItems } = useSelector((state) => state.cart);
 
 	const sumarItems = cartItems.reduce((sum, value) => sum + value.items, 0);
 	const dispatch = useDispatch();
-
 	const history = useHistory();
 
 	const [values, handleInputChange, reset] = useForm({ name: '' });
 	const { name } = values;
-
+	//TODO: botón de scroll en la posición 0 de windows
+	/* 	window.scroll({
+		top: 100,
+		left: 0,
+		behavior: 'smooth',
+	});
+ */
 	const handleSearch = (e) => {
 		e.preventDefault();
 		if (name.trim().length < 1) {
 			return;
 		}
-
 		dispatch(filterSearchMenu(name));
-		reset();
 		history.push(`/search/${name}`);
+		setActiveOpacity(!activeOpacity);
+		setBtnSearch(!btnSearch);
+		reset();
 	};
 
-	const [btnMenu, setBtnMenu] = useState(false);
-	const [btnSearch, setBtnSearch] = useState(false);
+	const handleBtnMan = () => {
+		setBtnMenu(!btnMenu);
+		setActiveOpacity(!activeOpacity);
+	};
+	const handleBtnWoman = () => {
+		setBtnMenu(!btnMenu);
+		setActiveOpacity(!activeOpacity);
+	};
 
-	const handleBtnMan = () => setBtnMenu(!btnMenu);
-	const handleBtnWoman = () => setBtnMenu(!btnMenu);
-
-	const handleBtnSearch = () => setBtnSearch(!btnSearch);
-	const handleBtnMenu = () => setBtnMenu(!btnMenu);
+	const handleBtnSearch = () => {
+		setBtnSearch(!btnSearch);
+		setBtnMenu(false);
+		setActiveOpacity(!activeOpacity);
+		if (btnMenu) {
+			setActiveOpacity(true);
+		}
+	};
+	const handleBtnMenu = () => {
+		setBtnMenu(!btnMenu);
+		setBtnSearch(false);
+		setActiveOpacity(!activeOpacity);
+		if (btnSearch) {
+			setActiveOpacity(true);
+		}
+	};
+	const handleOpacity = () => {
+		setBtnMenu(false);
+		setBtnSearch(false);
+		setActiveOpacity(false);
+	};
 	return (
-		<div className="nav__container">
-			<nav className="nav">
-				<figure className="nav__logo">
-					<Link to="/">
-						<img
-							src="../assets/logos/logo-white2.png"
-							alt="Logo"
-							className="nav__logo-img"
-						/>
+		<>
+			<div className={`${activeOpacity && 'block-relleno'}`}></div>
+			<div className={`nav__container ${activeOpacity && 'fixed'}`}>
+				<nav className="nav">
+					<figure className="nav__logo">
+						<Link to="/">
+							<img
+								src="../assets/logos/logo-white2.png"
+								alt="Logo"
+								className="nav__logo-img"
+							/>
+						</Link>
+					</figure>
+					<button onClick={handleBtnSearch} className="nav__btn-search">
+						<i className="fas fa-search"></i>
+						<span className="btn__search-text">Buscar</span>
+					</button>
+					<Link to="/cart" className="nav__btn-store">
+						<span className="store__indicator">{sumarItems}</span>
+						<i className="fas fa-cart-plus"></i>
+						<span className="nav__store-car">Carrito</span>
 					</Link>
-				</figure>
-				<button onClick={handleBtnSearch} className="nav__btn-search">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="22"
-						height="22"
-						fill="currentColor"
-						className="bi bi-search"
-						viewBox="0 0 16 16"
-					>
-						<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-					</svg>
-				</button>
 
-				<NavLogout />
+					<NavLogout />
 
-				<Link to="/cart" className="nav__btn-store">
-					<span className="store__indicator">{sumarItems}</span>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="22"
-						height="22"
-						fill="currentColor"
-						className="bi bi-cart3"
-						viewBox="0 0 16 16"
-					>
-						<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-					</svg>
-				</Link>
-				<div className="nav__categories">
-					<span className="nav__cat-text" onClick={handleBtnMenu}>
-						Categorías
-					</span>
-					<div className={`nav__menu ${btnMenu && 'activeMenu'}`}>
-						<Link
-							to="/woman"
-							onClick={handleBtnWoman}
-							className="nav__menu-link"
-							style={{ textDecoration: 'none' }}
-						>
-							Moda mujer
-						</Link>
-						<Link
-							to="/man"
-							onClick={handleBtnMan}
-							className="nav__menu-link radius--bottom "
-							style={{ textDecoration: 'none' }}
-						>
-							Moda hombre
-						</Link>
+					<div className="nav__categories">
+						<span className="nav__cat-text" onClick={handleBtnMenu}>
+							Categorías
+						</span>
+
+						<div className="btn__menu" onClick={handleBtnMenu}>
+							<div className="menu-activador">
+								<div className={`menu__ancla ${btnMenu && 'menu-abierto'}`}>
+									<span className="menu-activador-linea"></span>
+									<span className="menu-activador-linea"></span>
+									<span className="menu-activador-linea"></span>
+								</div>
+							</div>
+						</div>
+
+						<div className={`nav__menu ${btnMenu && 'activeMenu'}`}>
+							<Link
+								to="/woman"
+								onClick={handleBtnWoman}
+								className="nav__menu-link"
+								style={{ textDecoration: 'none' }}
+							>
+								Moda mujer
+							</Link>
+							<Link
+								to="/man"
+								onClick={handleBtnMan}
+								className="nav__menu-link radius--bottom "
+								style={{ textDecoration: 'none' }}
+							>
+								Moda hombre
+							</Link>
+						</div>
 					</div>
-				</div>
-			</nav>
+				</nav>
 
-			<form
-				onSubmit={handleSearch}
-				className={`nav__search ${btnSearch && 'activeSearch'}`}
-			>
-				<div className="nav__search-container">
-					<input
-						className="nav__search-input"
-						placeholder="¿Qué estas buscando?"
-						autoComplete="off"
-						type="text"
-						name="name"
-						value={name}
-						onChange={handleInputChange}
-					/>
-				</div>
-			</form>
-
-			{displayName && (
-				<div style={{ backgroundColor: 'white', padding: '5px' }}>
-					Bievenida {displayName}
-				</div>
-			)}
-		</div>
+				<form
+					onSubmit={handleSearch}
+					className={`nav__search ${btnSearch && 'activeSearch'}`}
+				>
+					<div className="nav__search-container">
+						<input
+							className="nav__search-input"
+							placeholder="¿Qué estas buscando?"
+							autoComplete="off"
+							type="text"
+							name="name"
+							value={name}
+							onChange={handleInputChange}
+						/>
+					</div>
+				</form>
+			</div>
+			<div
+				className={`${activeOpacity && 'opacity'}`}
+				onClick={handleOpacity}
+			></div>
+		</>
 	);
 };
