d5d9960b68531243687d8c9dfc274e650e87966e
Sergio Nivin <nivin684@gmail.com>
2021-09-17 08:43:02 -0500

    añadiendo permanencia de datos al componente clothesPagination

--- src/Components/clothes/ClothesList.jsx
+++ src/Components/clothes/ClothesList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -8,9 +8,37 @@ import { ClothesPaginationList } from './ClothesPaginationList';
 export const ClothesList = () => {
 	const { clothes } = useSelector((state) => state.filter);
 
+	const pageStorage = Number(localStorage.getItem('pageState') || 1);
+
+	const [pageState, setPageState] = useState(pageStorage);
+
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pageState]);
+
+	const clothesScreen = 12;
+
+	const clothesData = clothes.slice(
+		(pageState - 1) * clothesScreen,
+		pageState * clothesScreen
+	);
+
+	//cambiar el estado al inicial al superar el número de bloques de paginación y no muestre información
+	useEffect(() => {
+		if (clothesData.length === 0) {
+			localStorage.setItem('pageState', 1);
+			setPageState(1);
+		}
+	}, [clothesData]);
+
+	useEffect(() => {
+		localStorage.setItem('pageState', 1);
+		setPageState(1);
+	}, [clothes]);
 
 	return (
 		<>
@@ -20,11 +48,16 @@ export const ClothesList = () => {
 			{clothes.length !== 0 && (
 				<div className="container__grid_list">
 					<div className="grid animate__animated animate__fadeIn animate__faster">
-						{clothes.map((clothes) => (
+						{clothesData.map((clothes) => (
 							<ClothesCard key={clothes.id} {...clothes} />
 						))}
 					</div>
-					<ClothesPaginationList />
+					<ClothesPaginationList
+						page={pageState}
+						setPageState={setPageState}
+						clothesScreen={clothesScreen}
+						clothesTotal={clothes.length}
+					/>
 				</div>
 			)}
 		</>
--- src/Components/clothes/ClothesPaginationList.jsx
+++ src/Components/clothes/ClothesPaginationList.jsx
@@ -1,20 +1,59 @@
 import React from 'react';
 
-export const ClothesPaginationList = () => {
+export const ClothesPaginationList = ({
+	page,
+	setPageState,
+	clothesScreen,
+	clothesTotal,
+}) => {
+	const pagesTotal = Math.ceil(clothesTotal / clothesScreen);
+
+	const getBlocksPagination = () => {
+		const resultpages = [];
+		for (let i = 1; i <= pagesTotal; i++) {
+			resultpages.push(
+				<span
+					key={i}
+					onClick={() => {
+						localStorage.setItem('pageState', i);
+						setPageState(i);
+					}}
+					className={`pagination__block ${page === i && 'activate'}`}
+				>
+					{i}
+				</span>
+			);
+		}
+		return resultpages;
+	};
+
 	return (
 		<div className="list__pagination">
 			<div className="list__pagination-container">
-				<span className="pagination__decrement">
+				<span
+					className="pagination__decrement"
+					onClick={() => {
+						if (page !== 1) {
+							localStorage.setItem('pageState', page - 1);
+							setPageState(page - 1);
+						}
+					}}
+				>
 					<i className="fas fa-chevron-left"></i>Anterior
 				</span>
+
 				<div className="pagination__container-block">
-					<span className="pagination__block activate">1</span>
-					<span className="pagination__block">2</span>
-					<span className="pagination__block">3</span>
-					<span className="pagination__block">4</span>
+					{getBlocksPagination()}
 				</div>
-				<span className="pagination__increment">
-					Siguiente<i class="fas fa-chevron-right"></i>
+
+				<span
+					className="pagination__increment"
+					onClick={() => {
+						localStorage.setItem('pageState', page + 1);
+						setPageState(page + 1);
+					}}
+				>
+					Siguiente<i className="fas fa-chevron-right"></i>
 				</span>
 			</div>
 		</div>
